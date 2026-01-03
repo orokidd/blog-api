86a1afd9a20daf5addeb72b87e757873b3fb4542
orokidd <gregomily@gmail.com>
2026-01-03 11:49:57 +0800

    controller to get logged in user data

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -67,7 +67,28 @@ const register = async (req, res) => {
     }
 }
 
+const getLoggedUser = async (req, res) => {
+    try {
+        const user = await prisma.user.findUnique({
+            where: {id: req.user.id},
+            select: {
+                id: true,
+                email: true,
+                username: true,
+                role: true
+            }
+        })
+
+    if (!user) return res.status(400).json({message: 'User not found.'})
+    res.status(200).json(user)
+
+    } catch (error) {
+        res.status(500).json({message: 'Server error'})
+    }
+}
+
 module.exports = {
     login,
-    register
+    register,
+    getLoggedUser
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const authController = require('../controllers/auth.controller')
+const middleware = require('../middleware/auth.middleware')
 const router = express.Router()
 
 router.post('/login', authController.login)
 router.post('/register', authController.register)
+router.get('/logged-user', middleware.loginAuthenticate, authController.getLoggedUser)
 
 module.exports = router
