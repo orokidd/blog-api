4a4099bdc1f2283cfa5fb03f668508daf6c935c0
orokidd <gregomily@gmail.com>
2026-01-04 09:20:20 +0800

    small refactor

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -7,7 +7,7 @@ const login = async (req, res) => {
 
     try {
         const user = await prisma.user.findUnique({
-        where: { username: username }
+            where: { username: username }
         })
     
         if (!user) return res.status(400).json({ message: "Invalid username or password" })
